30eba7ef840be4d248787cf7003179d243c46795
Matthew Deadman <matthew.deadman@gmail.com>
2017-07-27 21:08:06 -0400

    Landing Page Banner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react'
 import EventList from './event/EventList.jsx'
 import VenueList from './venue/VenueList.jsx'
 import OrganizationList from './organization/OrganizationList.jsx'
+import homeStyle from '../../style/home.css'
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to Queen's Events!</h1>
+        <div className="landingPageBanner">
+          <h1>Welcome to Queen's Events!</h1>
+          <div className="landingButtons">
+            <button className="landingButton">Sign Up</button>
+            <button className="landingButton">Login</button>
+          </div>
+        </div>
+
         <EventList />
         <OrganizationList />
         <VenueList />
--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,15 @@ module.exports = {
     loaders: [
       { test: /\.css$/, loader: "style-loader!css-loader" },
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
 }
-
