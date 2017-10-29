00365914172835c2342832ab8071d2bb677b54a6
PfeifferH <15hp19@queensu.ca>
2017-10-29 17:28:52 -0400

    Page footer is now a component
    -Created Footer.jsx, which contains the code for the page footer
    
    -Changed App.jsx so that footer code is no longer contained, and implements Footer.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 import Main from './Main.jsx';
 import appStyle from '../../style/app.css';
 
@@ -12,18 +13,7 @@ class App extends Component {
           <Main />
         </div>
 
-        <div className="landingPageFooter">
-          <div className="landingPageFooterText">
-            <div>
-              <ul className="landingPageFooterTextList">
-                <li>© 2017 Queens Events</li>
-                <li><a href="https://www.facebook.com/QueensEvents/">Facebook</a></li>
-                <li><a href="https://www.instagram.com/queensevents/">Instagram</a></li>
-                <li><a href="https://twitter.com/eventsqueens">Twitter</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <Footer />
 
       </div>
     );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <div className="landingPageFooter">
+            <div className="landingPageFooterText">
+                <div>
+                    <ul className="landingPageFooterTextList">
+                        <li>© 2017 Queens Events</li>
+                        <li><a href="https://www.facebook.com/QueensEvents/">Facebook</a></li>
+                        <li><a href="https://www.instagram.com/queensevents/">Instagram</a></li>
+                        <li><a href="https://twitter.com/eventsqueens">Twitter</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer
