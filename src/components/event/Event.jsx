99fe6862b72997ac2117623664b4d5eaacaf9238
PfeifferH <15hp19@queensu.ca>
2017-11-04 15:05:31 -0400

    Removed Preexisting form and event header

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -6,21 +6,19 @@ import EventAdd from './EventAdd.jsx'
 import eventStyle from '../../../style/event.css'
 
 const Event = () => {
+
+
+
   return(
     <div>
-    <div className="eventPageBanner">
-        <h1>Ready to get started?</h1>
-        <h1>Add an Event</h1>
-        <div className="landingButtons">
+    <div>
+       <div className="landingButtons" >
           <Link to='/events/add'><button className="landingButton">Add Event</button></Link>
-          <Link to='/events/detail'><button className="landingButton">Event Detail</button></Link>
         </div>
       </div>
     <Switch>
       <Route exact path='/events' component={EventList}/>
       <Route path='/events/add' component={EventAdd}/>
-      <Route path='/events/:eventID' component={EventDetail}/>
-      <Route path='/events/detail' component={EventDetail}/>
     </Switch>
     </div>
   );
--- src/components/event/EventAdd.jsx
+++ src/components/event/EventAdd.jsx
@@ -3,7 +3,7 @@ import DayPickerInput from 'react-day-picker/DayPickerInput'
 import api from '../../http/api.js';
 
 import "react-day-picker/lib/style.css"
-import "../../../style/eventAdd.css"
+{/*import "../../../style/eventAdd.css"*/ }
 
 class EventAdd extends Component {
   constructor(props) {
@@ -53,71 +53,7 @@ class EventAdd extends Component {
         >
         
         
-        <table className="flexbox">
-            <tr>
-                <td className="col1">
-                    <div className="headings">
-                        <h2>Event Info</h2>
-                    </div>
-                    <br />
-                     
-                    <div className="element">Title:</div>
-
-                    <div className="textWrapper">
-                        <input className="textbox"
-                           value={this.state.title}
-                           onChange={event => this.setState({title: event.target.value})} />
-                    </div>
-                    <br />
-  
-                    <div className="element">Date:</div>
-
-                    <div className="textWrapper">
-                        <DayPickerInput className="textbox" placeholder="DD/MM/YYYY" format="DD/MM/YYYY" onDayClick={this.handleDayClick}/>
-                        <br />
-                    </div>
         
-                    <div className="element">Description:</div>
-
-                    <div className="textWrapper">
-                        <textarea className="textbox"
-                            value={this.state.description}
-                            onChange={event => this.setState({description: event.target.value})}
-                            cols="50" rows="10" />
-                        <br />
-                    </div>
-       
-                </td>
-        
-            <td className="col2">
-
-                <div className="headings">
-                    <h2>URLs</h2>       
-                </div>
-                <br />
-
-                <div className="element">Event URL:</div>
-
-                <div className="textWrapper">
-                    <input className="textbox"
-                        value={this.state.item_url}
-                        onChange={event => this.setState({item_url: event.target.value})} />
-                </div>
-
-                <div className="element">FB Event URL:</div>
-
-                <div className="textWrapper">
-                    <input className="textbox"
-                        value={this.state.fb_event_url}
-                        onChange={event => this.setState({fb_event_url: event.target.value})} />
-                    </div>
-        
-                <div className="submitWrapper">
-                    <button className="submitLink">Submit Event</button>
-                </div>
-            </td>
-       </tr>
-    </table>
     </form>
     );
   }
