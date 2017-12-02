ee82537756d33941d41b7339cd297e706a0e0f9c
PfeifferH <15hp19@queensu.ca>
2017-12-02 12:26:44 -0500

    Implemented skeleton layout from design doc

---
+++ src/components/user/ProfileEdit.jsx
@@ -0,0 +1,46 @@
+﻿import React, { Component } from 'react'
+import '../../../style/profileForm.css'
+
+class ProfileEdit extends Component {
+
+  render() {
+
+    return (
+
+      <form className="profileChangeContainer">
+
+        <div className="leftContainer">
+          
+          <h1 className="formTitle">Change Profile Info</h1>
+
+          <div className="formElement" id="firstName">
+            <h2>First Name</h2>
+            <input />
+          </div>
+
+          <div className="formElement" id="lastName">
+            <h2>Last Name</h2>
+            <input />
+          </div>
+
+          <div className="formElement" id="email">
+            <h2>Email</h2>
+            <input />
+          </div>
+
+          <div className="formElement" id="phoneNumber">
+            <h2>Phone</h2>
+            <input placeholder="XXX-XXX-XXXX" />
+          </div>
+
+        </div>
+
+        <div className="rightContainer">
+          Change Password
+        </div>
+      </form>
+    )
+  }
+}
+
+export default ProfileEdit
--- src/components/user/ProfilePage.jsx
+++ src/components/user/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom'
+import ProfileEdit from './ProfileEdit.jsx'
 import EventList from '../event/EventList.jsx'
-import VenueList from '../venue/VenueList.jsx'
 import OrganizationList from '../organization/OrganizationList.jsx'
 import cardStyle from '../../../style/profile.css'
 
@@ -11,19 +11,20 @@ class ProfilePage extends Component {
       <div className="profileContainer">
         <div className="profileUserInfo">
           <div className="profileUserImage" />
-          <h1>Stephen Peterkins</h1>
-          <p>Date joined</p>
+          <h1>Name</h1>
+          <p>Profile Description </p>
         </div>
         <div className="profileData">
           <ul>
-            <li><Link to='/profile/my-events'>My Events</Link></li>
-            <li><Link to='/profile/my-organizations'>My Organizations</Link></li>
-            <li><Link to='/profile/my-venues'>My Venues</Link></li>
+            <li className="profileElement"><Link to='/profile/edit-profile'>My Profile</Link></li>
+            <li className="profileElement"><Link to='/profile/my-events'>My Events</Link></li>
+            <li className="profileElement"><Link to='/profile/my-organizations'>My Organizations</Link></li>
           </ul>
+
           <Switch>
             <Route exact path='/profile/my-events' component={EventList} />
             <Route exact path='/profile/my-organizations' component={OrganizationList} />
-            <Route exact path='/profile/my-venues' component={VenueList} />
+            <Route exact path='/profile/edit-profile' component={ProfileEdit} />
           </Switch>
         </div>
       </div>
