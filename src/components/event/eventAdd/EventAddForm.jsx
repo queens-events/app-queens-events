5d04318c980803d16ef4610564396b7d25f40e32
PfeifferH <15hp19@queensu.ca>
2018-03-08 20:42:34 -0500

    Cleaned up eventAdd css, introducing css grid styling

--- src/components/event/eventAdd/EventAddForm.jsx
+++ src/components/event/eventAdd/EventAddForm.jsx
@@ -41,19 +41,9 @@ class EventAddForm extends Component {
             </div>
           </div>
 
-          <div className="createEventForm">
-            <div id="eventName">
-              <h1>Title:</h1>
-              <div>
-                <input
-                  name="name"
-                  type="text"
-                  value={newEvent.name}
-                  onChange={onChange} />
-              </div>
-            </div>
-
-            <div id="eventName">
+          <div className="createEventBody">
+           
+            <div id="eventImport">
               <h1>Import with Facebook:</h1>
               <div>
                 <input
@@ -67,17 +57,26 @@ class EventAddForm extends Component {
                   className="landingButton">Import</button>
               </div>
             </div>
+          
+            <div id="eventName">
+              <h1>Title:</h1>
+              <div>
+                <input
+                  name="name"
+                  type="text"
+                  value={newEvent.name}
+                  onChange={onChange} />
+              </div>
+            </div>
 
-            <div id="eventPhoto">
-              <h1>Event Photo:</h1>
+            <div id="eventImage">
+              <h1>Event Image:</h1>
               <div>
                 <Dropzone onDrop={(files) => onDrop(files)}>
                   <div>Try dropping some files here, or click to select files to upload.</div>
                 </Dropzone>
               </div>
             </div>
-
-            <div id="photoView"></div>
 
             <div id="eventDescription">
               <h1>Description:</h1>
@@ -92,52 +91,67 @@ class EventAddForm extends Component {
               </div>
             </div>
 
-            <div id="horizontal">
-            
-              <div id="eventCategory">
-                <h1>Category:</h1>
-                <div>
-                  <select name="category" value={newEvent.category} onChange={onChange}>
-                    <option value="HEALTH">Health</option>
-                    <option value="EDUCATION">Education</option>
-                    <option value="SPORTS">Sports</option>
-                    <option value="CONCERTS">Concerts</option>
-                    <option value="ARTS_AND_THEATER">Arts and Theater</option>
-                    <option value="SOCIALS">Socials</option>
-                  </select>
-                </div>
+            <div id="eventCategory">
+              <h1>Category:</h1>
+              <div>
+                <select name="category" value={newEvent.category} onChange={onChange}>
+                  <option value="HEALTH">Health</option>
+                  <option value="EDUCATION">Education</option>
+                  <option value="SPORTS">Sports</option>
+                  <option value="CONCERTS">Concerts</option>
+                  <option value="ARTS_AND_THEATER">Arts and Theater</option>
+                  <option value="SOCIALS">Socials</option>
+                </select>
               </div>
-
-              <div id="eventTags">
-                <h1>Tags:</h1>
-                <div>
-                  <select name="tags" value={newEvent.tag} onChange={onChange}>
-                    <option value="19+_SOCIALS">19+</option>
-                    <option value="ALL_AGES">All Ages</option>
-                    <option value="FREE">Free</option>
-                  </select>
-                </div>
-              </div>
-
-              <div id="eventCost">
-                <h1>Cost ($):</h1>
-                <div>
-                    <input name="cost" value={newEvent.cost} onChange={onChange} />
-                </div>
-              </div>
-
-              <div id="eventVenue">
-                <h1>Venue:</h1>
-                <div>
-                  <select name="venue" value={newEvent.venue} onChange={onChange}>
-                    <option value="Venue1">Venue1</option>
-                    <option value="Venue2">Venue2</option>
-                    <option value="Venue3">Venue3</option>
-                  </select>
-                </div>
-              </div>
-            
             </div>
+
+            <div id="eventTags">
+              <h1>Tags:</h1>
+              <div>
+                <select name="tags" value={newEvent.tag} onChange={onChange}>
+                  <option value="19+_SOCIALS">19+</option>
+                  <option value="ALL_AGES">All Ages</option>
+                  <option value="FREE">Free</option>
+                </select>
+              </div>
+            </div>
+
+            <div id="eventRecurrence">
+              <h1>Event Recurrence</h1>
+              <div>
+                <select name="recurrence" value={newEvent.recurrence} defaultValue="None" onChange={onChange}>
+                  <option value="None">None</option>
+                  <option value="Weekly">Weekly</option>
+                  <option value="Daily">Daily</option>
+                </select>
+              </div>
+            </div>
+
+            <div id="eventVenue">
+              <h1>Venue:</h1>
+              <div>
+                <select name="venue" value={newEvent.venue} onChange={onChange}>
+                  <option value="Venue1">Venue1</option>
+                  <option value="Venue2">Venue2</option>
+                  <option value="Venue3">Venue3</option>
+                </select>
+              </div>
+            </div>
+
+            <div id="eventCost">
+              <h1>Cost ($):</h1>
+              <div>
+                <input name="cost" value={newEvent.cost} onChange={onChange} size="4" />
+              </div>
+            </div>
+
+            <div id="eventDate">
+              <h1>Date:</h1>
+              <div>
+                <DayPickerInput placeholder="DD/MM/YYYY" format="DD/MM/YYYY" onDayChange={onChangeDatePicker} value={newEvent.date} />
+              </div>
+            </div>            
+
             <div id="eventStart">
               <h1>Starting Time:</h1>
               <div>
@@ -165,31 +179,21 @@ class EventAddForm extends Component {
                   />
               </div>
             </div>
+          </div>
 
-            <div id="eventDate">
-              <h1>Date:</h1>
-              <div>
-                <DayPickerInput placeholder="DD/MM/YYYY" format="DD/MM/YYYY" onDayChange={onChangeDatePicker} value={newEvent.date} />
-              </div>
+          <div className="createEventFooter">
+            <div id="eventPreview">
+              <button
+                type="button"
+                onClick={togglePreviewEventForm}
+                className="landingButton">Preview</button>
             </div>
 
-            <div id="eventRecurrence">
-              <h1>Event Recurrence</h1>
-              <div>
-                <select name="recurrence" value={newEvent.recurrence} defaultValue="None" onChange={onChange}>
-                  <option value="None">None</option>
-                  <option value="Weekly">Weekly</option>
-                  <option value="Daily">Daily</option>
-                </select>
-              </div>
-            </div>
-
-            <div id="submitDate">
-              <button type="button"
-                  onClick={togglePreviewEventForm}
-                  className="landingButton">Preview</button>
-              <button type="submit" className="landingButton">Submit Event</button>
-            </div>
+            <div id="eventSubmit">
+              <button
+                type="submit"
+                className="landingButton">Submit Event</button>
+             </div>
           </div>
         </form>
       </div>
